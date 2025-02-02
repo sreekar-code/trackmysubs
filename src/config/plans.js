95138b426b063bfbe909c7565e29e6e9ddcc5aec
sreekar-code <sreekar.writer@gmail.com>
2025-02-02 14:16:25 +0530

    I apo

---
+++ src/config/plans.js
@@ -0,0 +1,17 @@
+export const SUBSCRIPTION_PLANS = {
+  FREE: {
+    id: 'free',
+    name: 'Free',
+    price: 0,
+    features: [
+      'Unlimited subscriptions',
+      'Custom categories',
+      'Advanced analytics',
+      'Priority support',
+      'Export capabilities',
+      'Custom notifications',
+      'Budget tracking'
+    ],
+    stripeProductId: null
+  }
+}; 
